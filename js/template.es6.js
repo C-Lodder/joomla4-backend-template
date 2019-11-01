b4efe025aca3916576784523f02a11a59686b72b
C-Lodder <lodder15@hotmail.co.uk>
2019-11-01 13:26:58 +0000

    Fix navbar dropdown

--- js/template.es6.js
+++ js/template.es6.js
@@ -9,31 +9,32 @@
   document.addEventListener('DOMContentLoaded', () => {
 
     // Navbar
-    // const menu = document.getElementById('menu');
-    // document.querySelectorAll('#menu > .nav-item').forEach((item) => {
-      // const observer = new MutationObserver((mutation) => {
-        // if (Array.from(mutation[0].target.classList).includes('show')) {
-          // menu.classList.add('navbar-hover');
-		// } else {
-          // menu.classList.remove('navbar-hover');
-		// }
-      // });
-
-      // observer.observe(item, {
-        // attributes: true,
-        // attributeFilter: ['class']
-      // });
-    // });
-	
-    const cpanelAccordion = document.getElementById('cpanelAccordion');
-    if (cpanelAccordion) {
-      const firstCpanelAccordion = cpanelAccordion.querySelector('.card');
-      firstCpanelAccordion.querySelectorAll('[aria-expanded]').forEach((aria) => {
-        aria.setAttribute('aria-expanded', true);
+    const menu = document.getElementById('menu');
+    document.querySelectorAll('#menu > .nav-item').forEach((item) => {
+      const observer = new MutationObserver((mutation) => {
+        if (Array.from(mutation[0].target.classList).includes('show')) {
+          menu.classList.add('navbar-hover');
+		} else {
+          menu.classList.remove('navbar-hover');
+		}
       });
 
-      firstCpanelAccordion.querySelector('.collapse').classList.add('show');
-    }
+      observer.observe(item, {
+        attributes: true,
+        attributeFilter: ['class']
+      });
+    });
+	
+	// Accordion
+    // const cpanelAccordion = document.getElementById('cpanelAccordion');
+    // if (cpanelAccordion) {
+      // const firstCpanelAccordion = cpanelAccordion.querySelector('.card');
+      // firstCpanelAccordion.querySelectorAll('[aria-expanded]').forEach((aria) => {
+        // aria.setAttribute('aria-expanded', true);
+      // });
+
+      // firstCpanelAccordion.querySelector('.collapse').classList.add('show');
+    // }
 
   });
 
