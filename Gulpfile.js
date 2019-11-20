03135e1017e405a6d53952b151dca879c3f406b3
Charlie Lodder <charlie.lodder@semafone.com>
2019-11-20 15:37:43 +0000

    Improve Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -11,7 +11,7 @@ const cssnano      = require('cssnano');
 sass.compiler = require('node-sass');
 
 // Compile the SCSS
-gulp.task('sass', () =>
+gulp.task('sass-core', () =>
 	gulp.src([
 		`./scss/template.scss`,
 		`./scss/fonts/fontawesome.scss`,
@@ -23,23 +23,20 @@ gulp.task('sass', () =>
 		.pipe(gulp.dest(`./css`))
 );
 
-gulp.task('sass-joomla-installer', () =>
-	gulp.src(`./scss/joomla/installer.scss`)
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest(`./css/com_installer`))
-);
+// Vendor and other CSS overrides
+gulp.task('sass-vendor', async () => {
+	const files = {
+		'./scss/joomla/installer.scss'  : './css/com_installer',
+		'./scss/vendor/minicolors.scss' : './css/vendor/minicolors',
+		'./scss/vendor/choices.scss'    : './css/vendor/choicesjs',
+	};
 
-gulp.task('sass-vendor-minicolors', () =>
-	gulp.src(`./scss/vendor/minicolors.scss`)
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest(`./css/vendor/minicolors`))
-);
-
-gulp.task('sass-vendor-choices', () =>
-	gulp.src(`./scss/vendor/choices.scss`)
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest(`./css/vendor/choicesjs`))
-);
+	return Object.entries(files).forEach(([file, dist]) => {
+		return gulp.src(`${file}`)
+			.pipe(sass().on('error', sass.logError))
+			.pipe(gulp.dest(`${dist}`))
+	});
+});
 
 // Additional tasks to run on the compiled CSS file
 gulp.task('postcss', () =>
@@ -52,9 +49,7 @@ gulp.task('postcss', () =>
 );
 
 gulp.task('build', gulp.series(
-	'sass',
-	'sass-joomla-installer',
-	'sass-vendor-minicolors',
-	'sass-vendor-choices',
+	'sass-core',
+	'sass-vendor',
 	'postcss',
 ));
