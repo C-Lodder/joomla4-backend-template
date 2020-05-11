8fba03eb8aa48550bb3246056fa0a2f8da3f2500
C-Lodder <lodder15@hotmail.co.uk>
2020-05-11 17:32:26 +0100

    Minify JS

---
+++ build-js.js
@@ -0,0 +1,34 @@
+const { Worker, isMainThread, parentPort } = require('worker_threads')
+const { readdir, readFile, writeFile } = require('fs').promises
+const { resolve } = require('path')
+const Terser = require('terser')
+
+async function* recursiveSearch(dir) {
+  const dirents = await readdir(dir, { withFileTypes: true })
+  for (const dirent of dirents) {
+    const res = resolve(dir, dirent.name)
+    if (dirent.isDirectory()) {
+      yield* recursiveSearch(res)
+    } else {
+      yield res
+    }
+  }
+}
+
+async function processJs() {
+  for await (const file of recursiveSearch(`${__dirname}/js/`)) {
+    readFile(file, { encoding: 'utf8' })
+      .then((data) => {
+        writeFile(`${file.substr(0, file.lastIndexOf('.'))}.min.js`, Terser.minify(data).code)
+      })
+  }
+}
+
+if (isMainThread) {
+  const worker = new Worker(__filename)
+  worker.postMessage('message')
+} else {
+  parentPort.once('message', () => {
+    processJs()
+  })
+}
