7f596534e440f429732a0ed53fb07d2ba69cd167
C-Lodder <lodder15@hotmail.co.uk>
2020-05-22 07:50:42 +0100

    Update downloadurl when release script is run

--- build-release.js
+++ build-release.js
@@ -13,7 +13,7 @@ const updateJoomlaAssetJson = async () => {
 
 const updateTemplateXml = async () => {
   const file = await promises.readFile('./templateDetails.xml', 'utf8')
-  const xml = file.replace(/<version>([0-9.]+)<\/version>/g, `<version>${pkg.version}</version>`)
+  const xml = await file.replace(/<version>([0-9.]+)<\/version>/g, `<version>${pkg.version}</version>`)
 
   await promises.writeFile('./templateDetails.xml', xml)
   console.log('File "templateDetails.xml" has been updated')
@@ -21,7 +21,14 @@ const updateTemplateXml = async () => {
 
 const updateUpdateScript = async () => {
   const file = await promises.readFile('./updates/bettum_updates.xml', 'utf8')
-  const xml = file.replace(/<version>([0-9.]+)<\/version>/g, `<version>${pkg.version}</version>`)
+  let xml
+  try {
+    xml = await file
+      .replace(/<version>([0-9.]+)<\/version>/g, `<version>${pkg.version}</version>`)
+      .replace(/v([0-9.]+)\/tpl_bettum-v([0-9.]+)\.zip/g, `v${pkg.version}/tpl_bettum-v${pkg.version}.zip`)
+  } catch (error) {
+    console.log(error)
+  }
 
   await promises.writeFile('./updates/bettum_updates.xml', xml)
   console.log('File "bettum_updates.xml" has been updated')
@@ -29,8 +36,7 @@ const updateUpdateScript = async () => {
 
 const updateChangelog = async () => {
   const file = await promises.readFile('./CHANGELOG.md', 'utf8')
-  const markdown = file.replace(/##\sWIP/g, `## ${pkg.version}`)
-
+  const markdown = await file.replace(/##\sWIP/g, `## ${pkg.version}`)
   await promises.writeFile('./CHANGELOG.md', markdown)
   console.log('File "CHANGELOG.md" has been updated')
 }
