c5cc5c64383c6a5ec2ad1792ca6eae21ebb062eb
C-Lodder <lodder15@hotmail.co.uk>
2021-01-17 09:23:42 +0000

    Fix profile edit link

--- html/mod_user/default.php
+++ html/mod_user/default.php
@@ -23,7 +23,7 @@ $hideLinks = $app->input->getBool('hidemainmenu');
 	</button>
 	<div class="dropdown-menu dropdown-menu-right">
 		<div class="dropdown-header"><?php echo $user->name; ?></div>
-		<a class="dropdown-item" href="<?php echo Route::_('index.php?option=com_users&task=user.edit&id=' . $user->id . '&return=' . base64_encode($uri)); ?>">
+		<a class="dropdown-item" href="<?php echo Route::_('index.php?option=com_admin&task=profile.edit&id=' . $user->id . '&return=' . base64_encode($uri) . '#attrib-user_details'); ?>">
 			<svg aria-hidden="true" width="1em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M313.6 304c-28.7 0-42.5 16-89.6 16-47.1 0-60.8-16-89.6-16C60.2 304 0 364.2 0 438.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-25.6c0-74.2-60.2-134.4-134.4-134.4zM400 464H48v-25.6c0-47.6 38.8-86.4 86.4-86.4 14.6 0 38.3 16 89.6 16 51.7 0 74.9-16 89.6-16 47.6 0 86.4 38.8 86.4 86.4V464zM224 288c79.5 0 144-64.5 144-144S303.5 0 224 0 80 64.5 80 144s64.5 144 144 144zm0-240c52.9 0 96 43.1 96 96s-43.1 96-96 96-96-43.1-96-96 43.1-96 96-96z"/></svg>
 			<?php echo Text::_('MOD_USER_EDIT_ACCOUNT'); ?>
 		</a>
